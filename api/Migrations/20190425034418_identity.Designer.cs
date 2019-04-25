3adce7362d49044374af1cdea098d4da96b9a2f5
Heath Monroe <monroe.1150@buckeyemail.osu.edu>
2019-04-25 01:23:00 -0400

    Added Security Modules

--- api/Migrations/20190425034418_identity.Designer.cs
+++ api/Migrations/20190425034418_identity.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using drinkfinder.Models;
@@ -9,9 +10,10 @@ using drinkfinder.Models;
 namespace Drinkfinder.Api.Migrations
 {
     [DbContext(typeof(DrinkFinderContext))]
-    partial class DrinkFinderContextModelSnapshot : ModelSnapshot
+    [Migration("20190425034418_identity")]
+    partial class identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
