bfc88df6b86c060006927a7bff8017249b078a03
Matthew Kane <46544172+MatthewKane1995@users.noreply.github.com>
2019-04-26 19:03:15 -0400

    Update Startup.cs

--- api/Startup.cs
+++ api/Startup.cs
@@ -1,17 +1,21 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using drinkfinder.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace drinkfinder.Api
 {
@@ -29,7 +33,27 @@ namespace drinkfinder.Api
         {
             services.AddDbContext<DrinkFinderContext>(options => 
             options.UseNpgsql(Configuration.GetConnectionString("DrinkFinderContext")));
+
+            //Identity
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<DrinkFinderContext>().AddDefaultTokenProviders();
             
+            services.AddAuthentication(option =>
+            {
+//Changed line from DefaultAuthenticationScheme to DefaultScheme
+            option.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(jwtOptions =>
+            {
+                jwtOptions.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateActor = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidIssuer = Configuration["JWTConfiguration:Issuer"],
+                    ValidAudience = Configuration["JWTConfiguration:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWTConfiguration.Key"]))
+                };
+            });
             services.AddCors(options => {
                 options.AddPolicy("CorsPolicy", builder => 
                 {
@@ -43,15 +67,18 @@ namespace drinkfinder.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            //Idnetity
+            app.UseAuthentication();
+
+            // if (env.IsDevelopment())
+            // {
+            //     app.UseDeveloperExceptionPage();
+            // }
+            // else
+            // {
+            //     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //     app.UseHsts();
+            // }
 
         
             app.UseHttpsRedirection();
