e0414fb69b1975fda343146e2c8501999ff2e418
Heathm32 <46544180+Heathm32@users.noreply.github.com>
2019-04-26 18:31:20 -0400

    Update DrinkFinderContext.cs

--- api/Data/DrinkFinderContext.cs
+++ api/Data/DrinkFinderContext.cs
@@ -3,7 +3,7 @@ using Npgsql;
 
 namespace drinkfinder.Models
 {
- public class DrinkFinderContext : IdentityDbContext<ApplicationUser>
+
     {
         public DrinkFinderContext(DbContextOptions<DrinkFinderContext> options) : base(options)
 
